ae06026b8f7749f65f53ba86ad95a96276347ebd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-25 22:39:01 +0100

    ice: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: xiaomi Model: ice Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ice.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ice-user \
+    omni_ice-userdebug \
+    omni_ice-eng
